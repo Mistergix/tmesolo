0c0f8531efdac4a0c03a0d60cce39bb48ab07c8e
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-04-08 16:17:30 +0200

    rm useless stuff

--- training.py
+++
@@ -1,9 +0,0 @@
-from module_teo_nicolas.lib.qsoccer import QSoccer
-
-nb_player_per_team = 4
-qsoccer = QSoccer.get_instance(nb_player_per_team)
-
-exit()
-
-qsoccer.Train(show=False)
-qsoccer.printBestsAndWorsts()
